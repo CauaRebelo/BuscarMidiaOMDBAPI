c3dfca6342e9f12470b19bcdbdc4a4abc0322f7d
Cauã Rebelo <cauarebelo@discente.ufg.br>
2023-10-20 15:36:53 -0300

    Gerenciador_omdbb criado com funções de pesquisa e começando os favoritos.

---
+++ gerenciador_omdb.py
@@ -0,0 +1,72 @@
+import json
+import requests
+import os
+
+
+class GerenciadorOMDB:
+
+    def __init__(self, api_key):
+        self.api_key = api_key
+
+    def pesquisa_filmes(self, titulo, pagina):
+        try:
+            url = f"https://www.omdbapi.com/?apikey={self.api_key}&s={titulo}&page={pagina}"
+            resposta = requests.get(url)
+            if resposta.status_code == 200:
+                return resposta.json()
+            else:
+                print(f"Erro ao buscar dados JSON. Status code: {resposta.status_code}")
+                return None
+        except Exception as e:
+            print(f"Erro na solicitação: {e}")
+            return None
+
+    def exibir_titulos(self, data):
+        if not data:
+            return
+        filmes = data.get("Search", [])
+        imdb_ids = {}
+        for i, filme in enumerate(filmes):
+            titulo = filme.get("Title")
+            imdb_id = filme.get("imdbID")
+            imdb_ids[i] = imdb_id
+            print(f"{i}: {titulo}")
+
+        return imdb_ids
+
+    def exibir_detalhes(self, id_escolhido):
+        try:
+            url = f"https://www.omdbapi.com/?apikey={self.api_key}&i={id_escolhido}"
+            resposta = requests.get(url)
+
+            if resposta.status_code == 200:
+                midia_escolhida = resposta.json()
+                print("Detalhes da midia selecionada:")
+                print(f"Titulo: {midia_escolhida.get('Title')}")
+                print(f"Ano de Lancamento: ", midia_escolhida.get('Year'))
+                print(f"Classificacao: ", midia_escolhida.get('Rated'))
+                print(f"Sinopse: ", midia_escolhida.get('Plot'))
+                print(f"Diretor: ", midia_escolhida.get('Director'))
+                return resposta.json()
+        except Exception as e:
+            print(f"Erro na solicitação: {e}")
+            return None
+
+    def salvar_favoritos(self, nome_pasta, data):
+        if not data:
+            return
+        os.makedirs(nome_pasta, exist_ok=True)
+        print(f"imdbID: ", data.get('imdbID'))
+        path = f"/{nome_pasta}/{data.get('imdbID')}.json"
+        if not os.path.exists(path):
+            try:
+                with open(path, 'w') as arquivo_filme:
+                    json.dump(data, arquivo_filme)
+                arquivo_filme.close()
+                print("Salvo como favoritos com sucesso!")
+                return
+            except IOError:
+                print("Nao foi possivel criar o arquivo.")
+                return
+        print("O arquivo ja esta salvo como favoritos.")
+        return
--- main.py
+++ main.py
@@ -1,40 +1,9 @@
 # This is a sample Python script.
 
 import json
-
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
-
-caminho_fav = "galaxy.json"
-
-def abrir_favoritos():
-    try:
-        with open(caminho_fav, "r") as arquivo_json:
-            # Carregue o conteúdo do arquivo em uma estrutura de dados Python
-            favoritos = json.load(arquivo_json)
-
-    except FileNotFoundError:
-        print(f"O arquivo '{caminho_galaxy}' não foi encontrado.")
-    except json.JSONDecodeError as e:
-        print(f"Erro ao decodificar o JSON: {e}")
-
-    # Agora você pode trabalhar com os dados como uma estrutura de dados Python
-    print("Conteúdo do arquivo JSON:")
-    for i, item in enumerate(favoritos, 1):
-        print(f"{i}: {item['Title']}")
-
-    try:
-        escolha = int(escolha)
-        if 1 <= escolha <= len(dados):
-            # Exiba os detalhes do item escolhido
-            item_escolhido = dados[escolha - 1]
-            print("Detalhes do item escolhido:")
-            for chave, valor in item_escolhido.items():
-                print(f"{chave}: {valor}")
-        else:
-            print("Escolha inválida. Por favor, escolha um número válido.")
-    except ValueError:
-        print("Escolha inválida. Por favor, insira um número.")
+from gerenciador_omdb import GerenciadorOMDB
+import favoritos
+import requests
 
 def print_hi(name):
     # Use a breakpoint in the code line below to debug your script.
@@ -44,7 +13,44 @@ def print_hi(name):
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
     print_hi('PyCharm')
-    abrir_favoritos()
+    apikey = "7eade183"
+    titulo = ""
+    nome_pasta = "favoritos"
+    pagina = 1
 
+    titulo = input("Qual titulo deseja buscar? ")
+    pesquisador = GerenciadorOMDB(apikey)
+
+    while(1):
+        dados_json = pesquisador.pesquisa_filmes(titulo, pagina)
+        if dados_json:
+            imdb_ids = pesquisador.exibir_titulos(dados_json)
+            print("Escolha 'n' ou 'p' para ir para a proxima pagina ou anterior, ou 'q' para sair. Ou:")
+            escolha = input("Escolha um número para ver os detalhes: ")
+            if escolha == 'q':
+                print("Saindo da pesquisa.")
+                break
+            elif escolha == 'n':
+                pagina+=1
+            elif escolha == 'p':
+                pagina -=1
+            else:
+                try:
+                    index = int(escolha)
+                    imdb_id_escolhido = imdb_ids[index]
+                    dados_json = pesquisador.exibir_detalhes(imdb_id_escolhido)
+                    escolha = input("Escolha 'f' para salvar como favoritos ou 'l' para voltar para a lista: ")
+                    if escolha == 'l':
+                        print("Voltando a lista.")
+                    elif escolha == 'f':
+                        pesquisador.salvar_favoritos(nome_pasta, dados_json)
+                        escolha = input("Escolha 'l' para voltar a lista ou 'q' para sair: ")
+                        if escolha == 'l':
+                            print("Voltando a lista.")
+                        elif escolha == 'q':
+                            print("Saindo da pesquisa.")
+                            break
+                except ValueError:
+                    print("Escolha inválida. Insira um número válido.")
 
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
